41655a1a553f52ac5ae1f2ee31a83d790c4e38c2
Ritsuki Goto <develop@mizucoffee.net>
2021-06-23 12:29:25 +0000

    [add] models

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+require './models'
 
 get '/' do
   erb :index
---
+++ models.rb
@@ -0,0 +1,4 @@
+ActiveRecord::Base.establish_connection
+class Post < ActiveRecord::Base
+
+end
