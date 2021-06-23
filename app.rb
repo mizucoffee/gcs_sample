c4b2a08433f5c322f346c4fdb3d951542d7806d7
Ritsuki Goto <develop@mizucoffee.net>
2021-06-23 16:40:11 +0000

    [add] erb & gcs init

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
 require './models'
+require 'google/cloud/storage'
+
+storage = Google::Cloud::Storage.new(
+  credentials: "./credentials.json"
+)
+
+bucket = storage.bucket "gcs-sample-posts"
 
 get '/' do
   erb :index
